6da98650da6ccd2917b29d265cfaba361b05e716
Cecilia Brito Santos <ceciliabritosantos@gmail.com>
2022-01-25 20:11:08 -0300

    adicionando função para somar os números e filtrando valores undefined

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-//TO-DO Adicionar Observer que consiga somar, depois adicionar observer para as outras operações
+//TO-DO Adicionar observer para as outras operações
+//e refatorar codigo: melhorar nomes de variáveis, constantes e funções,
 
-const operations = { calculateAddition: () => {
+const operations = { calculateAddition: (command) => {
 		getValuesOfDisplay();
-		// dateDisplay.result += parseInt(dateDisplay.result, 10);
-		// dateDisplay.number = dateDisplay.result
-		// console.log('Result', result)
+		if(command.buttonPressed == '+'){
+			dateDisplay.result += parseInt(dateDisplay.number, 10);
+			console.log('Result', dateDisplay.result)
+		} else if(command.buttonPressed == '='){
+			dateDisplay.number = parseInt(dateDisplay.result, 10) + parseInt(dateDisplay.number, 10)
+			dateDisplay.result = parseInt(dateDisplay.number, 10)
+			console.log('Result', dateDisplay.result)
+			console.log('number', dateDisplay.number)
+		} 
 	}
+}
+
+function calculate(command){
+	operations.calculateAddition(command);
+	addResultInDisplay(command)
 }
 
 
@@ -14,14 +26,12 @@ const DISPLAY = document.querySelector('[data-display-number]')
 const DISPLAY_PART_TO_OPERATION = document.querySelector('[data-display-operation]')
 
 function addValuesInDisplay(buttonPressed){
-	if(buttonPressed != '+' && buttonPressed != '-' && buttonPressed != '/' && buttonPressed != 'X' && buttonPressed != undefined && buttonPressed != '='){
+	if(buttonPressed != '+' && buttonPressed != '-' && buttonPressed != '/' && buttonPressed != 'X' && buttonPressed != undefined && buttonPressed != '=' && buttonPressed != 'undefined'){
 		DISPLAY.innerHTML += buttonPressed
 		
-	} else if(buttonPressed != undefined){
+	} else if(buttonPressed != undefined && command.buttonPressed != 'undefined'){
 		DISPLAY.innerHTML = ''
 		DISPLAY_PART_TO_OPERATION.innerHTML = buttonPressed
-		addResultInDisplay(buttonPressed)
-		dateDisplay.result =  '';
 	}
 	console.log(dateDisplay);
 }
@@ -48,7 +58,7 @@ const factoryButtonListenner = () => {
 	calculator.addEventListener('click', createButtonListenner);
 
 	function createButtonListenner(event){
-		const buttonPressed = event.target.textContent;
+		const buttonPressed = event.target.dataset.value + '';
 		console.log(buttonPressed);
 		addValuesInDisplay(buttonPressed)
 
@@ -73,10 +83,10 @@ function getValuesOfDisplay(){
 	console.log('datedisplay ', dateDisplay.number)
 }
 
-function addResultInDisplay(equalPressed){
-	if(equalPressed == '='){
-		DISPLAY.innerHTML = dateDisplay.result 
-		dateDisplay.number = dateDisplay.result
+function addResultInDisplay(buttonPressed){
+	if(buttonPressed.buttonPressed == '='){
+		DISPLAY.textContent = dateDisplay.result 
+		dateDisplay.result = 0;
 	}
 }
 
@@ -94,4 +104,4 @@ function factoryMembersOfOperation(){
 
 var dateDisplay =  factoryMembersOfOperation();
 var buttonListenner = factoryButtonListenner();
-// buttonListenner.subscribe(operations.calculateAddition);
+buttonListenner.subscribe(calculate);
