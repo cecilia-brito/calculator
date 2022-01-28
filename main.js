e6720cf47628fef0223b1485d126b8fc9cb0ce97
Cecilia Brito Santos <ceciliabritosantos@gmail.com>
2022-01-28 11:13:19 -0300

    consertando bug e soma, multiplicação, divisão é subtração de números inteiros funcionando

--- main.js
+++ main.js
@@ -1,39 +1,74 @@
-//TO-DO Adicionar observer para as outras operações
-//e refatorar codigo: melhorar nomes de variáveis, constantes e funções,
-
-const operations = { calculateAddition: (command) => {
-		getValuesOfDisplay();
-		if(command.buttonPressed == '+'){
-			dateDisplay.result += parseInt(dateDisplay.number, 10);
-			console.log('Result', dateDisplay.result)
-		} else if(command.buttonPressed == '='){
-			dateDisplay.number = parseInt(dateDisplay.result, 10) + parseInt(dateDisplay.number, 10)
-			dateDisplay.result = parseInt(dateDisplay.number, 10)
-			console.log('Result', dateDisplay.result)
-			console.log('number', dateDisplay.number)
-		} 
-	}
-}
-
-function calculate(command){
-	operations.calculateAddition(command);
-	addResultInDisplay(command)
-}
-
-
+// TO-DO refatorar codigo: melhorar nomes de variáveis, constantes e funções,
+var adicionou = false;
+var multiplicou = false;
+var dividiu = false;
+var subtraiu = false;
 
 const DISPLAY = document.querySelector('[data-display-number]')
 const DISPLAY_PART_TO_OPERATION = document.querySelector('[data-display-operation]')
 
+const operations = { 
+	calculateAddition: (command) => {
+		getValuesOfDisplay();
+		if(command.buttonPressed == '+'){
+			dataDisplay.result = 0;
+			dataDisplay.result += dataDisplay.number
+			adicionou = true;
+		} else if((command.buttonPressed === '=') && (adicionou === true)){
+			dataDisplay.number = dataDisplay.result + dataDisplay.number
+			dataDisplay.result = dataDisplay.number
+			adicionou = false
+			addResultInDisplay(command)
+		} 
+	}, calculateMultiplication: (command) => {
+			getValuesOfDisplay();
+			if(command.buttonPressed == 'X'){
+				dataDisplay.result = 1;
+				dataDisplay.result *= dataDisplay.number
+				multiplicou = true;
+			} else if(command.buttonPressed == '='  && (multiplicou == true)){
+				dataDisplay.number = dataDisplay.result * dataDisplay.number
+				dataDisplay.result = dataDisplay.number
+				multiplicou = false;
+				addResultInDisplay(command);
+			} 
+	}, calculateDivision: (command) => {
+			getValuesOfDisplay();
+			if(command.buttonPressed == '/'){
+				dataDisplay.result = 1;
+				dataDisplay.result = dataDisplay.number
+				dividiu = true
+			} else if(command.buttonPressed == '=' && (dividiu == true)){
+				dataDisplay.number = dataDisplay.result / dataDisplay.number
+				dataDisplay.result = dataDisplay.number
+				dividiu = false
+				addResultInDisplay(command);
+			} 
+	}, calculteSubtration: (command) => {
+			getValuesOfDisplay();
+			if(command.buttonPressed == '-'){
+				dataDisplay.result = 0 ;
+				dataDisplay.result = dataDisplay.number
+				subtraiu = true
+			} else if((command.buttonPressed == '=') && (subtraiu == true)){
+				dataDisplay.number = dataDisplay.result - dataDisplay.number
+				dataDisplay.result = dataDisplay.number
+				subtraiu = false
+				addResultInDisplay(command)
+			} 
+		}
+}
+
 function addValuesInDisplay(buttonPressed){
 	if(buttonPressed != '+' && buttonPressed != '-' && buttonPressed != '/' && buttonPressed != 'X' && buttonPressed != undefined && buttonPressed != '=' && buttonPressed != 'undefined'){
+		DISPLAY.innerHTML = ''
 		DISPLAY.innerHTML += buttonPressed
 		
-	} else if(buttonPressed != undefined && buttonPressed != 'undefined'){
-		DISPLAY.innerHTML = ''
+	} else if(buttonPressed != undefined && buttonPressed != 'undefined' && buttonPressed != '.'){
 		DISPLAY_PART_TO_OPERATION.innerHTML = buttonPressed
+		// DISPLAY.innerHTML = 0
 	}
-	console.log(dateDisplay);
+	console.log('dataDisplay addValuesInDisplay: ', dataDisplay);
 }
 
 const factoryButtonListenner = () => {
@@ -60,11 +95,12 @@ const factoryButtonListenner = () => {
 	function createButtonListenner(event){
 		const buttonPressed = event.target.dataset.value + '';
 		console.log(buttonPressed);
-		addValuesInDisplay(buttonPressed)
 
 		const command = {
 			buttonPressed: buttonPressed
 		}
+
+		addValuesInDisplay(buttonPressed)
 
 		notifyAll(command)
 		return buttonPressed;
@@ -78,15 +114,16 @@ const factoryButtonListenner = () => {
 
 function getValuesOfDisplay(){
 	for(let i = 0; i < DISPLAY.textContent.length; i++){
-		dateDisplay.number = DISPLAY.textContent;
+		dataDisplay.number = parseInt(DISPLAY.innerHTML);
+		// DISPLAY.innerHTML = 0;
 	}
-	console.log('datedisplay ', dateDisplay.number)
+	console.log('datedisplay ', dataDisplay.number)
 }
 
 function addResultInDisplay(buttonPressed){
-	if(buttonPressed.buttonPressed == '='){
-		DISPLAY.textContent = dateDisplay.result 
-		dateDisplay.result = 0;
+	if(buttonPressed.buttonPressed === '='){
+		DISPLAY.innerHTML = parseInt(dataDisplay.result) 
+		dataDisplay.result = '';
 	}
 }
 
@@ -94,14 +131,18 @@ function addResultInDisplay(buttonPressed){
 function factoryMembersOfOperation(){
 	
 	const membersOfOperation = {
-		number: '',
-		operation: '',
-		result: 0
+		number: 0,
+		result: ''
 	}
 
 	return membersOfOperation;
 }
 
-var dateDisplay =  factoryMembersOfOperation();
+var dataDisplay =  factoryMembersOfOperation();
 var buttonListenner = factoryButtonListenner();
-buttonListenner.subscribe(calculate);
+
+//observers subscribes
+buttonListenner.subscribe(operations.calculateAddition);
+buttonListenner.subscribe(operations.calculateMultiplication);
+buttonListenner.subscribe(operations.calculateDivision);
+buttonListenner.subscribe(operations.calculteSubtration);
